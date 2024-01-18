ce47658f405dbcb9165907c9abae136c5360ef26
Arung Agamani <arung.agamani@gmail.com>
2024-01-18 21:07:28 +0800

    chore: change to npm feat: enable copypasta

--- src/bot/commands/copypasta.ts
+++ src/bot/commands/copypasta.ts
@@ -25,30 +25,21 @@ export class CopyPastaCommand extends Command {
             name: "copypasta",
             aliases: [""],
 
-            description:
-                "Gives random copypasta. Viewer discretion advised. Also not accepting arguments currently,..nyaa",
+            description: "Gives random copypasta. Viewer discretion advised. Also not accepting arguments currently,..nyaa",
         });
     }
 
     public override async messageRun(message: Message) {
-        if (message.author.id !== "145558597424644097") {
-            await message.channel.send("Bot owner level feature, for now...");
-            return;
-        }
-        const wangyRes = await axios.get<{}, AxiosResponse<WangyResponse[]>>(
-            "https://wangy.howlingmoon.dev/api/wangy"
-        );
+        // if (message.author.id !== "145558597424644097") {
+        //     await message.channel.send("Bot owner level feature, for now...");
+        //     return;
+        // }
+        const wangyRes = await axios.get<{}, AxiosResponse<WangyResponse[]>>("https://wangy.howlingmoon.dev/api/wangy");
         const validCopypastas = wangyRes.data.filter((item) => {
             if (item.tags.length === 0) return true;
             else return false;
         });
-        await message.channel.send(
-            (
-                validCopypastas[
-                    Math.floor(Math.random() * validCopypastas.length)
-                ]!.input as string
-            ).substring(0, 2000)
-        );
+        await message.channel.send((validCopypastas[Math.floor(Math.random() * validCopypastas.length)]!.input as string).substring(0, 2000));
         return;
     }
 }
